df3b2b0902af859dc1ca04f6818f2f4cb9d14fb7
Zakaria Davis <zakariadavis@gmail.com>
2020-01-23 16:16:20 +0300

    Lab 2 Part 1 Complete

---
+++ LAB2/DBConnector.php
@@ -0,0 +1,27 @@
+<?php
+define('DB_SERVER', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'btc3205');
+
+class DBConnector
+{
+    public $conn;
+
+    public function __construct()
+    {
+        /**
+         * mysql_connect function has been depricated and removed from PHP v7.0.0.
+         * https://www.php.net/manual/en/function.mysql-connect.php
+         *
+         */
+        $this->conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME) or die ("Could not connect to mysql");        
+
+    }
+    
+
+    public function closeDatabase()
+    {
+        mysqli_close($this->conn);
+    }
+}
---
+++ LAB2/lab1.php
@@ -0,0 +1,109 @@
+<?php
+include_once 'User.php';
+include_once 'DBConnector.php';
+
+if (isset($_POST['btn-save'])) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $city = $_POST['city_name'];
+    //Creating new user
+    $user = new User($first_name, $last_name, $city);
+
+    if(!$user->validateForm()){
+        $user->createFormErrorSessions();
+        header("Refresh:0");
+        die();
+    }
+
+    $res = $user->save();
+
+    //Check if user is successfully created
+
+    if ($res) {
+        echo "Save operation was successful";
+    } else {
+        echo "An error occured!";
+    }
+}else if(isset($_POST['btn-view-all'])){    
+        $displayTable = NULL;            
+        $users = User::readAll(); 
+        if ($users) {
+            $displayTable = $users;
+        } else {
+            echo "An error occured!";
+        }                      
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab 2</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="validate.css">
+</head>
+
+<body>
+<div class="container">
+<div>
+<h1>Create user</h1>
+<form method="post" name="user_details" id="user_details" onsubmit="return ValidateForm()" action="<?php $_SERVER['PHP_SELF'] ?>">
+        <table>
+            <tr>
+                <td>
+                    <div id="form-errors">
+                        <?php 
+                            session_start();
+                            if(!empty($_SESSION['form_errors'])){
+                                echo "" . $_SESSION['form_errors'];
+                                unset($_SESSION['form_errors']);
+                            }
+                        ?>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input value=""  placeholder="First Name" type="text"  name="first_name" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input value="" placeholder="Last Name" type="text" name="last_name">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input value="" placeholder="City name" type="text" name="city_name">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <button type="submit" name="btn-save">SAVE</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
+    <div>
+        <h1>Display Users</h1>
+        <form method="post">
+            <tr>
+                <td><button type="submit" name="btn-view-all">View All Entries</button></td>
+            </tr>
+        </form>
+        <?php
+        if (isset($displayTable)) {
+            echo $displayTable;
+        }
+        ?>
+    </div>
+</div>
+<script src="validate.js"></script>
+</body>
+
+</html>
